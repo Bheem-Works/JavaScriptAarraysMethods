1124e78ddc10cba7fa1e3687702c52aa9d355969
bheem-works <shokokomichan098@gmail.com>
2025-06-06 09:44:11 +0545

    this is the table summary of the useful array methods

---
+++ toString.js
@@ -0,0 +1,21 @@
+	
+
+	// This is the toString() Methods of the arrays;
+	// toString methods is used for to conver an array object to the 
+
+	// The examples of the toString() methods in the arrays are. 
+	let cat = ['miso','jiso','tiso'];
+	let catString = cat.toString();
+	console.log(catString);
+
+	// time show by using the toString Methods;
+	let time = new Date();
+	console.log(time.toString());
+
+
+	// Concat 
+	let copyCat = ['miso','jiso','limo'];
+	let copyCatOne = ['rone','prone','prob'];
+	let copyCatTwo = ['jesicca','miro','shifini'];
+	const concat = copyCat.concat(copyCatOne,copyCatTwo);
+	console.log(concat);	
