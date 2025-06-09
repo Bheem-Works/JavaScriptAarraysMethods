000c6d42c4adeee2943cbece093a16b89fd43ef0
bheem-works <shokokomichan098@gmail.com>
2025-06-09 11:23:52 +0545

    Introduction of the fetching data

---
+++ fetch.js
@@ -0,0 +1,7 @@
+
+
+	// This Code will be going to be An fetching asyncs and the promises methods with the basics examples. and defination;
+	
+	// Resolve : Resolve means when the data and value is sucessfully transfered; 
+	// Error : Error means when the value or the data cannot be fetched then the errors occurs;
+	// Promise : Promise is used to get the data value when the fetching is sucess. In other's words.
